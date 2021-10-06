a8cf7027a8320ed548708497515474f07872eb25
Isa Eken <hello@isaeken.com.tr>
2021-10-06 06:07:15 +0300

    update config

--- config/translator.php
+++ config/translator.php
@@ -1,9 +1,49 @@
 <?php
 // config for IsaEken/LaravelTranslator
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Enable in production
+    |--------------------------------------------------------------------------
+    |
+    | Enable package in production environment.
+    |
+    */
+
+    'production' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Supported Languages
+    |--------------------------------------------------------------------------
+    |
+    | List of supported languages.
+    |
+    */
+
     'supported_languages' => [
         'en' => 'English',
     ],
 
-    'abort_if_unsupported' => false,
+    /*
+    |--------------------------------------------------------------------------
+    | Auto save translations
+    |--------------------------------------------------------------------------
+    |
+    | Auto-add unattached translations.
+    |
+    */
+
+    'autosave' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Throw error undefined
+    |--------------------------------------------------------------------------
+    |
+    | throw an error when requesting a translation that hasn't been added.
+    |
+    */
+
+    'throw_undefined' => false,
 ];
