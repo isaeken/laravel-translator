0c8bf28181cad1f6648f61e84b2344f3fc535627
Isa Eken <hello@isaeken.com.tr>
2021-10-06 05:21:27 +0300

    wip

--- src/Translation/Translator.php
+++ src/Translation/Translator.php
@@ -2,53 +2,10 @@
 
 namespace IsaEken\LaravelTranslator\Translation;
 
+use IsaEken\LaravelTranslator\Exceptions\TranslationNotExistsException;
+
 class Translator extends \Illuminate\Translation\Translator
 {
-    /**
-     * @param  string  $key
-     * @param  array  $replace
-     * @param  string|null  $locale
-     * @param  bool  $fallback
-     *
-     * @return array|string
-     */
-    public function getBase($key, array $replace = [], string|null $locale = null, bool $fallback = true): array|string
-    {
-        $locale = $locale ?: $this->locale;
-
-        // For JSON translations, there is only one file per locale, so we will simply load
-        // that file and then we will be ready to check the array for the key. These are
-        // only one level deep so we do not need to do any fancy searching through it.
-        $this->load('*', '*', $locale);
-
-        $line = $this->loaded['*']['*'][$locale][$key] ?? null;
-
-        // If we can't find a translation for the JSON key, we will attempt to translate it
-        // using the typical translation file. This way developers can always just use a
-        // helper such as __ instead of having to pick between trans or __ with views.
-        if (!isset($line)) {
-            [$namespace, $group, $item] = $this->parseKey($key);
-
-            // Here we will get the locale that should be used for the language line. If one
-            // was not passed, we will use the default locales which was given to us when
-            // the translator was instantiated. Then, we can load the lines and return.
-            $locales = $fallback ? $this->localeArray($locale) : [$locale];
-
-            foreach ($locales as $locale) {
-                if (!is_null($line = $this->getLine(
-                    $namespace, $group, $locale, $item, $replace
-                ))) {
-                    return $line;
-                }
-            }
-        }
-
-        // If the line doesn't exist, we will return back the key which was requested as
-        // that will be quick to spot in the UI if language keys are wrong or missing
-        // from the application's language files. Otherwise we can return the line.
-        return $this->makeReplacements($line ?: $key, $replace);
-    }
-
     /**
      * Get or create the translation.
      *
@@ -59,18 +16,12 @@ class Translator extends \Illuminate\Translation\Translator
      *
      * @return string|array
      */
-    public function getOrNew(
-        string $key,
-        array $replace = [],
-        string|null $locale = null,
-        bool $fallback = true
-    ): string|array {
+    public function getOrNew(string $key, array $replace = [], string|null $locale = null, bool $fallback = true): string|array
+    {
         $locale = $locale ?: $this->locale;
-
         $this->load('*', '*', $locale);
-        $line = $this->loaded['*']['*'][$locale][$key] ?? null;
 
-        if ($line == null) {
+        if (!$this->has($key, $locale, false)) {
             $path = resource_path('lang/' . $this->fallback . '.json');
             $translations = [];
 
@@ -84,7 +35,7 @@ class Translator extends \Illuminate\Translation\Translator
             }
         }
 
-        return $this->getBase($key, $replace, $locale, $fallback);
+        return parent::get($key, $replace, $locale, $fallback);
     }
 
     /**
@@ -99,10 +50,14 @@ class Translator extends \Illuminate\Translation\Translator
      */
     public function get($key, array $replace = [], $locale = null, $fallback = true): array|string
     {
-        if (app()->environment() !== 'production') {
+        if (config('translator.throw_undefined', false) === true) {
+            throw_unless($this->has($key, $locale, false), TranslationNotExistsException::class, $key, $locale);
+        }
+
+        if (config('translator.autosave', false) === true) {
             return $this->getOrNew($key, $replace, $locale, $fallback);
         }
 
-        return $this->getBase($key, $replace, $locale, $fallback);
+        return parent::get($key, $replace, $locale, $fallback);
     }
 }
