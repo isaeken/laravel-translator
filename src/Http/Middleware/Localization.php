d38aa8dc11835fac54d0ea5eebe182fc4f115aee
Isa Eken <hello@isaeken.com.tr>
2021-10-06 06:44:25 +0300

    update middleware

--- src/Http/Middleware/Localization.php
+++ src/Http/Middleware/Localization.php
@@ -4,7 +4,6 @@ namespace IsaEken\LaravelTranslator\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use IsaEken\LaravelTranslator\LaravelTranslator;
 
 class Localization
 {
@@ -18,15 +17,10 @@ class Localization
      */
     public function handle(Request $request, Closure $next): mixed
     {
-        $locale = $request->input('locale') ?? ($request->header('Content-Language') ?? config('app.locale'));
-
-        if (config('translator.abort_if_unsupported')) {
-            abort_unless(LaravelTranslator::isSupported($locale), 403, 'Unsupported language');
+        if ($request->has('locale')) {
+            app()->setLocale($request->get('locale'));
         }
 
-        app()->setLocale($locale);
-        $response = $next($request);
-        $response->headers->set('Content-Language', $locale);
-        return $response;
+        return $next($request);
     }
 }
